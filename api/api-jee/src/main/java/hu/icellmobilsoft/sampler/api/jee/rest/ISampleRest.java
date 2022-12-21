6087b1e0983621c0cdbbf86c0be4f6a3cb4a4abc
Imre Scheffer <imre.scheffer@icellmobilsoft.hu>
2022-12-21 16:32:38 +0100

    #19 jakarta EE10 migration
    bom, dto, api

--- api/api-jee/src/main/java/hu/icellmobilsoft/sampler/api/jee/rest/ISampleRest.java
+++ api/api-jee/src/main/java/hu/icellmobilsoft/sampler/api/jee/rest/ISampleRest.java
@@ -19,12 +19,12 @@
  */
 package hu.icellmobilsoft.sampler.api.jee.rest;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
