3df263012ade26297acfa529c2f6cd5bee3dd6d6
Gábor Gyenge <gyengus@gmail.com>
2024-06-10 13:45:26 +0200

    #58 Add criteria sample

--- sample/sample-jpa-service/src/main/java/hu/icellmobilsoft/sampler/sample/jpaservice/service/SampleEntityService.java
+++ sample/sample-jpa-service/src/main/java/hu/icellmobilsoft/sampler/sample/jpaservice/service/SampleEntityService.java
@@ -19,13 +19,26 @@
  */
 package hu.icellmobilsoft.sampler.sample.jpaservice.service;
 
+import java.text.MessageFormat;
 import java.util.List;
+import java.util.Optional;
 
 import jakarta.enterprise.inject.Model;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+import org.apache.commons.lang3.StringUtils;
 
 import hu.icellmobilsoft.coffee.cdi.logger.AppLogger;
 import hu.icellmobilsoft.coffee.cdi.logger.ThisLogger;
+import hu.icellmobilsoft.coffee.cdi.trace.annotation.Traced;
+import hu.icellmobilsoft.coffee.dto.exception.InvalidParameterException;
+import hu.icellmobilsoft.coffee.dto.exception.TechnicalException;
+import hu.icellmobilsoft.coffee.dto.exception.enums.CoffeeFaultType;
 import hu.icellmobilsoft.coffee.se.api.exception.BaseException;
 import hu.icellmobilsoft.sampler.common.system.jpa.service.BaseService;
 import hu.icellmobilsoft.sampler.model.sample.SampleEntity;
@@ -51,11 +64,9 @@ public class SampleEntityService extends BaseService<SampleEntity> {
     /**
      * Elements associated with status
      *
-     * @param status
-     *            sample status
+     * @param status sample status
      * @return entity
-     * @throws BaseException
-     *             on error
+     * @throws BaseException on error
      */
     public List<SampleEntity> findAllByStatus(SampleStatus status) throws BaseException {
         return wrapListValidated(sampleEntityRepository::findAllByStatus, status, "findAllByStatus", "status");
@@ -65,13 +76,49 @@ public class SampleEntityService extends BaseService<SampleEntity> {
      * Find sample entity by id. It is for testing query method tracing.
      * <a href="https://github.com/i-Cell-Mobilsoft-Open-Source/coffee/issues/550">See coffee issue</a>
      *
-     * @param id
-     *            {@link SampleEntity#getId()}
+     * @param id {@link SampleEntity#getId()}
      * @return entity
-     * @throws BaseException
-     *             on error
+     * @throws BaseException on error
      */
     public SampleEntity findById(String id) throws BaseException {
         return wrapValidated(sampleEntityRepository::findById, id, "findById", "id");
+    }
+
+    /**
+     * Find sample entity by id. It is for testing criteria query method.
+     * @param id {@link SampleEntity#getId()}
+     * @param clazz Entity class
+     * @return {@link Optional<SampleEntity>}
+     * @throws BaseException on error
+     */
+    @Traced
+    public Optional<SampleEntity> findOptionalById(String id, Class<SampleEntity> clazz) throws BaseException {
+        if (StringUtils.isBlank(id) || clazz == null) {
+            log.warn("Entity Id is blank or clazz is null skipped to load!");
+            throw new InvalidParameterException("id is blank or clazz is null!");
+        }
+        log.trace(">> BaseService.findOptionalById(id: [{0}], class: [{1}])", id, clazz.getCanonicalName());
+        SampleEntity entity = null;
+        try {
+            EntityManager entityManager = getEntityManager();
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<SampleEntity> criteriaQuery = criteriaBuilder.createQuery(clazz);
+            Root<SampleEntity> root = criteriaQuery.from(SampleEntity.class);
+            criteriaQuery.where(criteriaBuilder.equal(root.get("id"), id));
+            TypedQuery<SampleEntity> query = entityManager.createQuery(criteriaQuery);
+            List<SampleEntity> results = query.getResultList();
+            if (!results.isEmpty()) {
+                entity = results.get(0);
+            } else {
+                log.debug("No result, id: [{0}].", id);
+            }
+        } catch (Exception e) {
+            String msg = MessageFormat.format("Error occured in finding class: [{0}] by id: [{1}]: [{2}]", clazz.getCanonicalName(), id, e.getLocalizedMessage());
+            log.error(msg, e);
+            throw new TechnicalException(CoffeeFaultType.REPOSITORY_FAILED, msg, e);
+        } finally {
+            log.trace("<< BaseService.findOptionalById(id: [{0}], class: [{1}])", id, clazz.getCanonicalName());
+        }
+        return Optional.ofNullable(entity);
     }
 }
